f7e1737b9e219f0c1f94e212963ee8146938bf44
Daniel <henio09@wp.pl>
2023-07-01 12:09:33 +0200

    adding todos implemented

---
+++ src/components/NewTodoInputField.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+	import { ref } from 'vue';
+
+	defineProps([
+		'hasTodos', 
+		'isActive', 
+		'handleToggleTodos',
+	]);
+
+  const emit = defineEmits(['handleAddNewTodo'])
+
+	const title = ref<string>('');
+
+	function handleAddNewTodo() {
+		emit('handleAddNewTodo', title.value);
+    title.value = '';
+	}
+
+</script>
+
+<template>
+	<header class="todoapp__header">
+        <button
+					v-if="hasTodos"
+          type="button"
+          :class="{'todoapp__toggle-all': true, 'active': isActive}"
+          
+        />
+
+      <form @submit="handleAddNewTodo">
+        <input
+          type="text"
+          class="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          v-model="title"
+        />
+      </form>
+    </header>
+</template>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
   import { ref } from 'vue';
-import { getTodos } from '../api/todos';
+import { addTodo, getTodos } from '../api/todos';
 import type { Todo } from '@/types/TodoItem';
 import TodoList from '@/components/TodoList.vue';
 import FooterMenu from '@/components/FooterMenu.vue';
+import NewTodoInputField from '@/components/NewTodoInputField.vue';
 
-  const todoList = ref<Todo[]>([]);
+  const todosList = ref<Todo[]>([]);
+  const tempTodo = ref<Todo>();
  
 
   const handleLoadTodos = () => {
     getTodos()
       .then(response => {
-        todoList.value = [...response];
+        todosList.value = [...response];
       })
       .catch(error => {
         console.log(error);
       });
+  };
+
+  const handleAddNewTodo = (
+    title: string,
+  ) => {
+    // event.preventDefault();
+    console.log('funct start');
+
+    if (title.trim() !== '') {
+      tempTodo.value = {
+        id: 0,
+        userId: 0,
+        title,
+        completed: false,
+      };
+
+      addTodo(tempTodo.value)
+      .then(response => {
+        todosList.value = [...todosList.value, response];
+      })
+      // .catch(() => setErrorType(Errors.ADD));
+      
+    } else {
+      // setErrorType(Errors.EMPTY_TITLE);
+    }
   };
 
   handleLoadTodos();
@@ -28,20 +55,18 @@ import FooterMenu from '@/components/FooterMenu.vue';
         <h1 className="todoapp__title">todos</h1>
 
         <div className="todoapp__content">
-          <!-- <NewTodoInputField
-            hasTodos={todosList.length > 0}
-            isActive={todosList.filter(todo => todo.completed)
-              .length === todosList.length}
-            handleAddNewTodo={handleAddNewTodo}
-            handleCompleteAll={handleCompleteAll}
-          /> -->
-         
+          <NewTodoInputField
+            :hasTodos="todosList.length > 0"
+            :isActive="todosList.filter(todo => todo.completed)
+              .length === todosList.length"
+            @handleAddNewTodo="handleAddNewTodo"
+          />
             <div v-if="true">
               <section className="todoapp__main">
-                <TodoList :todos="todoList" />
+                <TodoList :todos="todosList" />
               </section>
 
-              <FooterMenu :todos="todoList" />
+              <FooterMenu :todos="todosList" />
             </div>
          
         </div>
